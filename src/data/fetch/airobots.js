cf8ddb145732f487274c0c15010039e87b767443
kori <104796743+melankorin@users.noreply.github.com>
2026-03-22 20:10:28 -0300

    Add robots.txt

---
+++ src/data/fetch/airobots.js
@@ -0,0 +1,12 @@
+// Uncomment this and install dependency if using.
+// npm install @11ty/eleventy-fetch -D
+/* const EleventyFetch = require("@11ty/eleventy-fetch");
+
+module.exports = async function() {
+    const url = "https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/main/robots.txt";
+
+    return EleventyFetch(url, {
+        duration: "7d",
+        type: "text"
+    });
+}; */
