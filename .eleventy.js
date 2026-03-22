389461a945d7fdb49529f18b7e1992db9ec4b90e
kori <104796743+melankorin@users.noreply.github.com>
2026-03-22 15:22:00 -0300

    Add date and time global data and filter

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,25 @@ module.exports = function (eleventyConfig) {
     const yaml = require("js-yaml");
     eleventyConfig.addDataExtension("yaml,yml", (contents) => yaml.load(contents));
 
+    // Add current date and time as global data
+    eleventyConfig.addGlobalData("now", () => {
+        return new Date().toISOString();
+    });
+
+    // FILTERS ------------------------------------------------------------------------------------
+
+    // Date formatting filter
+    /*
+        To use this:
+        {{ variable | date_format: "d LLL yyy", "utc" }}
+    */
+    const { DateTime } = require("luxon");
+    eleventyConfig.addFilter("date_format", (date, format = "d LLL yyyy", timezone = "utc") => {
+        return DateTime.fromJSDate(new Date(date), { zone: timezone })
+            .setLocale("en")
+            .toFormat(format);
+    });
+
     // SERVE AND EXPORT ---------------------------------------------------------------------------
 
     // Dev server options
