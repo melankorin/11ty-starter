90930cceda279c568acc2ca4e7e2ccc02bf3c23b
kori <104796743+melankorin@users.noreply.github.com>
2026-03-22 21:12:42 -0300

    Set filters to strict in Liquid

--- .eleventy.js
+++ .eleventy.js
@@ -151,6 +151,11 @@ module.exports = function (eleventyConfig) {
     /* const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
     eleventyConfig.addPlugin(syntaxHighlight); */
 
+    // Strict filters in Liquid
+    eleventyConfig.setLiquidOptions({
+        strictFilters: true
+    });
+
     // SERVE AND EXPORT ---------------------------------------------------------------------------
 
     // Dev server options
