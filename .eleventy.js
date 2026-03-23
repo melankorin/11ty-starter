32ed95ec792fab7d9dfb6cd349ebda9832f3ae38
kori <104796743+melankorin@users.noreply.github.com>
2026-03-22 21:07:15 -0300

    Add image shortcode and pipeline

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,24 @@ module.exports = function (eleventyConfig) {
 
     // FILES --------------------------------------------------------------------------------------
 
-    // Copy assets
+    // Copy assets and static images
     eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy({ "src/img/static": "assets/img" });
 
     // Copy favicons
     eleventyConfig.addPassthroughCopy({"src/favicons/!(*.liquid)": "/"});
+
+    // Copy optimized images after build
+    const fs = require("fs-extra");
+    eleventyConfig.on("eleventy.after", async () => {
+        const source = "./.cache/images/";
+        const dest = "./site/assets/img/";
+
+        if (fs.existsSync(source)) {
+            await fs.copy(source, dest);
+            console.log(`${chalk.green("[Image Cache]")} Synced to ${dest}`);
+        }
+    });
 
     // DATA ---------------------------------------------------------------------------------------
 
@@ -46,6 +59,45 @@ module.exports = function (eleventyConfig) {
             ${content}
         </div>
         `
+    });
+
+    // Image optimization shortcode
+    /*
+        To use this:
+        {% image "src", "alt", "classes", "widths", "sizes", "formats", "loading", "decoding" %}
+        {% image "./src/img/dynamic/ocean.jpg", "A blue ocean", "w-64 h-64 object-cover border-4 border-white", "30, 600", "(max-width: 800px) 100vw, 50vw", "webp", "lazy", "async" %}
+    */
+    const Image = require("@11ty/eleventy-img");
+    const path = require("path");
+    const chalk = require("chalk")
+    eleventyConfig.addShortcode("image", async function (src, alt, className = "", widths = "300, 600", sizes = "100vw", formats = "webp", loading = "lazy", decoding = "async") {
+        const inputBase = "./src/img/dynamic/";
+        const outputBase = "./.cache/images/";
+        const relativeDir = path.relative(inputBase, path.dirname(src));
+        const widthArray = typeof widths === "string" 
+            ? widths.split(",").map(s => {
+                const val = s.trim();
+                return val.toLowerCase() === "auto" ? "auto" : parseInt(val, 10);
+            }) 
+            : widths;
+        const formatArray = typeof formats === "string" ? formats.split(",").map(s => s.trim()) : formats;
+        let metadata = await Image(src, {
+            widths: widthArray,
+            formats: formatArray,
+            outputDir: path.join(outputBase, relativeDir),
+            urlPath: path.join("/assets/img/", relativeDir).replace(/\\/g, '/'),
+            cacheOptions: {
+                duration: "*",
+                directory: ".cache/eleventy-img/",
+                removeFinished: false
+            },
+            filenameFormat: function (id, src, width, format) {
+                const extension = path.extname(src);
+                const name = path.basename(src, extension);
+                return `${name}-${width}.${format}`;
+            }
+        });
+        return Image.generateHTML(metadata, {alt, sizes, class: className, loading, decoding});
     });
     
     // MARKDOWN -----------------------------------------------------------------------------------
